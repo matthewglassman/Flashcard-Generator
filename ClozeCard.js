e2ed2757ff5442e88b4c0cec4aa2ea8bd8223892
matthewglassman <matthewglassmn78@gmail.com>
2017-02-20 21:05:15 -0500

    Creating constructor for Clozed Deletion Cards

---
+++ ClozeCard.js
@@ -0,0 +1,11 @@
+var inquirer = require("inquirer");
+var clozeCardArray = [];
+var cardCount = 0
+function ClozeCard(text, cloze){
+	this.text = text;
+	this.cloze = cloze;
+
+	this.partialText = function(){
+		partialText = (this.text)
+	}
+}
