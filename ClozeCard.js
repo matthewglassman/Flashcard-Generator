6985c0a1731c043a146af05c2e31c2bed598752d
matthewglassman <matthewglassmn78@gmail.com>
2017-02-20 23:15:04 -0500

    Working functionality.  Need to update the return based upon right or wrong answer

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,23 +1,18 @@
 var inquirer = require("inquirer");
 var clozeCardArray = [];
-var cardCount = 0
+var cardCount = 0;
+
 function ClozeCard(text, cloze){
-	
-	this.cardHandler = function(){
-		if (text.indexOf(close) === -1){
-			console.log("Sorry but " + cloze + " is not in the current text.")
-		} else {
 		this.text = text;
 		this.cloze = cloze;
 		this.partialText = this.text.replace(this.cloze, '...');
 
 		};
-	
-	};
-};
 
-clozeCardArray.push(new ClozeCard("George Washington was the first President of the United States?", "George Washington"), new ClozeCard("The most recent Super Bowl took place in the city of Houston?", "Houston"), new ClozeCard("2 + 2 = 4 ?", "4"), new clozeCardArray("A lemon is typically yellow in color.", "yellow")
+clozeCardArray.push(new ClozeCard("George Washington was the first President of the United States?", "George Washington"), new ClozeCard("The most recent Super Bowl took place in the city of Houston?", "Houston"), new ClozeCard("2 + 2 = 4 ?", "4"), new ClozeCard("A lemon is typically yellow in color.", "yellow")
 	);
+
+console.log(clozeCardArray);
 
 var askQuestion = function(){
 	if (cardCount < clozeCardArray.length){
@@ -27,16 +22,16 @@ var askQuestion = function(){
 			message: clozeCardArray[cardCount].partialText
 		}
 		]).then(function(answers){
-			var useranswer = answer.question;
+			var useranswer = answers.question;
 			var flashcardCloze = clozeCardArray[cardCount].cloze;
 
-			if(useranswer.toLowerCase() === flashcardCloze){
+			if(useranswer.toLowerCase() === flashcardCloze.toLowerCase()){
 				console.log("Yay!");
 				cardCount++;
 				askQuestion();
 			}else{
 				console.log("Oh too bad!");
-				cardCount++
+				cardCount++;
 				askQuestion();
 			}
 
